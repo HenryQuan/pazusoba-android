8fc3fbf905f128bb72a37dad7ee4148673624834
Yiheng <development.henryquan@gmail.com>
2021-09-11 23:33:16 +1000

    route is returned from C++
    it can be a bit slow, need to improve the speed of pazusoba

--- project/app/src/main/java/org/github/henryquan/pazusoba_android/MainActivity.kt
+++ project/app/src/main/java/org/github/henryquan/pazusoba_android/MainActivity.kt
@@ -10,6 +10,7 @@ import org.github.henryquan.pazusoba_android.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var solver: Pazusoba = Pazusoba()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +22,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun solve(view: android.view.View) {
+        val stepValue = binding.stepSlider.values.first()
+        val beamValue = binding.beamSizeSlider.values.first()
+
+        val maxStep = (50 * stepValue).toInt()
+        val beamSize = (beamValue * 10000).toInt()
+        solver = Pazusoba(3, maxStep, beamSize)
         solveBoard()
     }
 
@@ -28,12 +35,12 @@ class MainActivity : AppCompatActivity() {
      * Run in a thread so UI is not blocked while solving the board
      */
     private fun solveBoard() {
+        binding.sampleText.text = "Solving..."
         Thread {
-            val stepList = Pazusoba.solve("GDHRGRGRBDHLHRBDGGHLLBBDHGGLBB")
-            for (step in stepList) {
-                Log.d("pazusoba", "${step} =>")
+            val routeString = solver.solve("GDHRGRGRBDHLHRBDGGHLLBBDHGGLBB")
+            runOnUiThread {
+                binding.sampleText.text = routeString
             }
-            Log.d("pazusoba", "END")
         }.start()
     }
 }
--- project/app/src/main/java/org/github/henryquan/pazusoba_android/core/Pazusoba.kt
+++ project/app/src/main/java/org/github/henryquan/pazusoba_android/core/Pazusoba.kt
@@ -1,15 +1,52 @@
 package org.github.henryquan.pazusoba_android.core
 
-object Pazusoba {
-    // Load the shared library
-    init {
-        System.loadLibrary("pazusoba_android")
+import android.util.Log
+
+class Pazusoba(val minErase: Int = 3, val maxStep: Int = 30, val beamSize: Int = 2500) {
+
+    fun solve(board: String): String {
+        val start = System.currentTimeMillis()
+        val stepList = solve(board, minErase, maxStep, beamSize)
+        val size = stepList.count()
+        val column = getColumn(board.length)
+
+        var route = ""
+        for (step in stepList) {
+            val x = step / column
+            val y = step % column
+            route += "($x, $y) -> "
+        }
+        route += "END"
+
+        val end = System.currentTimeMillis() - start
+        route += "\nIt took $end ms\n$size step(s) in total\nBeam - $beamSize, Step - $maxStep"
+        return route
     }
 
-    external fun solve(
+    private fun getColumn(length: Int): Int {
+        return when (length) {
+            20 -> 5
+            30 -> 6
+            42 -> 7
+            else -> error("Unknown board size ${length}")
+        }
+    }
+
+    /**
+     * Native Methods
+     */
+
+    private external fun solve(
         board: String,
-        minErase: Int = 3,
-        maxStep: Int = 30,
-        beamSize: Int = 2500
+        minErase: Int,
+        maxStep: Int,
+        beamSize: Int,
     ): IntArray
+
+    // Load the shared library once only
+    companion object {
+        init {
+            System.loadLibrary("pazusoba_android")
+        }
+    }
 }
